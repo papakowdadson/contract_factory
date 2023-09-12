0a49d28cbc16fa2ca65e3f3894e97ccd80df231b
pee47kay <papakowdadson@gmail.com>
2023-09-12 11:10:38 +0000

    added voting factory

---
+++ blockchain/contracts/votingFactory.sol
@@ -0,0 +1,11 @@
+import "Voting" from './Voting.sol';
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+
+
+contract VotingFactory {
+    
+   Voting1 = new Voting(owner)
+        
+}
